22e3aacfd4ff8fe53a3233f8e5c90c58775988e4
Manavendra Sen <manavendra4288@gmail.com>
2022-05-20 02:19:19 +0530

    feat: add dice images

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,24 +3,33 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var resultImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
+        resultImage = findViewById(R.id.result_image)
         rollButton.setOnClickListener{
             rollDice()
         }
     }
 
     private fun rollDice() {
-        val resultText : TextView = findViewById(R.id.result_text)
-        val randomInt = Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
+        val drawableResource = when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        resultImage.setImageResource(drawableResource)
     }
 }
